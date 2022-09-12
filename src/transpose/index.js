af139a202a26fcad606d75bc33cf9174b1c138fd
Decagon <decagon@Decagons-MacBook-Air.local>
2022-09-12 16:31:06 +0100

    solution to all minichallenge 1 tasks

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,21 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+ function transpose(array) {
+    const number =[];
+
+    const row =array.length;
+    const col = array[0].length;
+    for(let i =0; i<col; i++){
+        number[i]= Array(row)
+    }
+    for(let j =0; j<row; j++){
+        for (let i=0; i<col; i++){
+            number[i][j] = array[j][i]
+        }
+    }
+    return number;
+}
+
 
 module.exports = transpose;
